90cb8ee856ebae6427a99582373f36cc3da13114
anitrazelenkamxg65 <141511123+anitrazelenkamxg65@users.noreply.github.com>
2023-08-05 14:44:06 -0400

    Update scraping_script.py

--- scraping_script.py
+++ scraping_script.py
@@ -1,72 +1,79 @@
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+%%time
 
-BASE_URL = 'https://www.truepeoplesearch.com'
-SEED_URL = 'https://www.truepeoplesearch.com/find/smith'
-PROXY_FILE = 'proxies.txt'
-PROCESSED_URLS_FILE = 'processed_urls.txt'
-OUTPUT_FILE = 'output.txt'
+n_pages = 0
 
-def load_proxies():
-    with open(PROXY_FILE, 'r') as f:
-        proxies = f.read().splitlines()
-    return proxies
+for page in range(0,900):
+    n_pages += 1
+    sapo_url = 'https://casa.sapo.pt/Venda/Apartamentos/?sa=11&lp=10000&or=10'+'&pn='+str(page)
+    r = get(sapo_url, headers=headers)
+    page_html = BeautifulSoup(r.text, 'html.parser')
+    house_containers = page_html.find_all('div', class_="searchResultProperty")
+    if house_containers != []:
+        for container in house_containers:
+            
+            # Price            
+            price = container.find_all('span')[2].text
+            if price == 'Contacte Anunciante':
+                price = container.find_all('span')[3].text
+                if price.find('/') != -1:
+                    price = price[0:price.find('/')-1]
+            if price.find('/') != -1:
+                price = price[0:price.find('/')-1]
+            
+            price_ = [int(price[s]) for s in range(0,len(price)) if price[s].isdigit()]
+            price = ''
+            for x in price_:
+                price = price+str(x)
+            prices.append(int(price))
 
-def save_processed_url(url):
-    with open(PROCESSED_URLS_FILE, 'a') as f:
-        f.write(url + '\n')
+            # Zone
+            location = container.find_all('p', class_="searchPropertyLocation")[0].text
+            location = location[7:location.find(',')]
+            zone.append(location)
 
-def is_processed(url):
-    with open(PROCESSED_URLS_FILE, 'r') as f:
-        processed_urls = f.read().splitlines()
-    return url in processed_urls
+            # Title
+            name = container.find_all('span')[0].text
+            titles.append(name)
 
-def scrape_page(url, proxies):
-    options = Options()
-    driver = webdriver.Chrome(options=options)
+            # Status
+            status = container.find_all('p')[5].text
+            condition.append(status)
 
-    try:
-        # Установить прокси, если доступны
-        if proxies:
-            proxy = proxies.pop(0)
-            options.add_argument('--proxy-server=http://{}'.format(proxy))
-        
-        driver.get(url)
+            # Area
+            m2 = container.find_all('p')[9].text
+            if m2 != '-':
+                m2 = m2.replace('\xa0','')
+                m2 = float("".join(itertools.takewhile(str.isdigit, m2)))
+                areas.append(m2)
+                
+            else:
+                m2 = container.find_all('p')[7].text
+                if m2 != '-':
+                    m2 = m2.replace('\xa0','')
+                    m2 = float("".join(itertools.takewhile(str.isdigit, m2)))
+                    areas.append(m2)
+                else:
+                    areas.append(m2)
 
-        # Дождитесь загрузки страницы
-        wait = WebDriverWait(driver, 10)
-        wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="your-element-id"]')))
+            # Creation date
+            date = pd.to_datetime(container.find_all('div', class_="searchPropertyDate")[0].text[21:31])
+            created.append(date)
 
-        # Получите содержимое элемента, который вы хотите скрапить
-        element = driver.find_element(By.XPATH, '//*[@id="your-element-id"]')
-        content = element.get_attribute('innerText')
+            # Description
+            desc = container.find_all('p', class_="searchPropertyDescription")[0].text[7:-6]
+            descriptions.append(desc)
 
-        # Теперь вы можете обработать содержимое элемента, например, записать его в файл
-        with open(OUTPUT_FILE, 'a', encoding='utf-8') as f:
-            f.write(content + '\n')
+            # url
+            link = 'https://casa.sapo.pt/' + container.find_all('a')[0].get('href')[1:-6]
+            urls.append(link)
 
-        # Сохраните URL в списке обработанных
-        save_processed_url(url)
-
-        # Найдите ссылки на странице и пройдите по ним рекурсивно
-        links = driver.find_elements(By.TAG_NAME, 'a')
-        for link in links:
-            href = link.get_attribute('href')
-            if href and href.startswith(BASE_URL) and not is_processed(href):
-                scrape_page(href, proxies)
-
-    except Exception as e:
-        print(f"Произошла ошибка при обработке страницы: {e}")
-
-    finally:
-        driver.quit()
-
-if __name__ == "__main__":
-    proxies = load_proxies()
-    scrape_page(SEED_URL, proxies)
+            # image
+            img = str(container.find_all('img')[0])
+            img = img[img.find('data-original_2x=')+18:img.find('id=')-2]
+            thumbnails.append(img)
+    else:
+        break
+    
+    sleep(randint(1,2))
+    
+print('You scraped {} pages containing {} properties.'.format(n_pages, len(titles)))
